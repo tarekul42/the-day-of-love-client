09ffc3ca7fdbc075a032296cb2d369054c685d77
tarekul42 <tarekulrifat205@gmail.com>
2025-02-22 00:00:03 +0600

    menubar on navbar position finding and fixed

--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -14,33 +14,39 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   const user = true;
   return (
-    <div className="absolute top-0 left-0 w-full bg-background border-b py-3 text-center text-muted-foreground shadow">
-      {user ? (
-        <DropdownMenu>
-          <DropdownMenuTrigger>
-            <Avatar>
-              <AvatarImage src="https://github.com/shadcn.png" />
-              <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent>
-            <Link to="/dashboard">
-              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+    <>
+      <div className="absolute top-0 left-0 w-full bg-background border-b py-3 text-center text-muted-foreground shadow">
+        <div>{/* the drrpdown and menubar will be addded here */}</div>
+
+        <div>
+          {user ? (
+            <DropdownMenu>
+              <DropdownMenuTrigger>
+                <Avatar>
+                  <AvatarImage src="https://github.com/shadcn.png" />
+                  <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <Link to="/dashboard">
+                  <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                </Link>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>Profile</DropdownMenuItem>
+                <DropdownMenuItem>Billing</DropdownMenuItem>
+                <DropdownMenuItem>Team</DropdownMenuItem>
+                <DropdownMenuItem>Subscription</DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          ) : (
+            <Link to="/authentication">
+              {" "}
+              <Button>Login</Button>
             </Link>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>Profile</DropdownMenuItem>
-            <DropdownMenuItem>Billing</DropdownMenuItem>
-            <DropdownMenuItem>Team</DropdownMenuItem>
-            <DropdownMenuItem>Subscription</DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      ) : (
-        <Link to="/authentication">
-          {" "}
-          <Button>Login</Button>
-        </Link>
-      )}
-    </div>
+          )}
+        </div>
+      </div>
+    </>
   );
 };
 
